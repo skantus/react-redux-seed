2a2a202f7dfb606b529e835f2dcba9d0704c1a53
Alejo Castaño <drskantus@hotmail.com>
2019-01-19 22:05:08 +0100

    Update home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import { func } from 'prop-types';
 import { connect } from 'react-redux';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import Details from 'components/Details';
-import { Increment, Decrement } from 'store/actions';
+import { Grid } from 'react-flexbox-grid';
+import LoginForm from 'components/LoginForm';
 import { content } from './home.module.css';
 
 class Home extends React.Component {
   render() {
-    const { Increment, Decrement } = this.props;
     return (
       <Grid fluid className={content}>
-        <Row>
-          <button onClick={Decrement}>Decrement</button>
-          <Col>
-            <Details />
-          </Col>
-          <button onClick={Increment}>Increment</button>
-        </Row>
+        <LoginForm />
       </Grid>
     );
   }
 }
 
-Home.propTypes = {
-  Increment: func.isRequired,
-  Decrement: func.isRequired
-};
-
-const mapDispatchToProps = {
-  Increment,
-  Decrement
-};
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Home);
+export default connect()(Home);

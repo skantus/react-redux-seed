132c9281dd622a3c0129504543beede749d5434c
Alejo Castaño <drskantus@hotmail.com>
2019-01-19 22:04:54 +0100

    Add LoginForm component

---
+++ src/components/LoginForm/styles.js
@@ -0,0 +1,12 @@
+export const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  input: {
+    margin: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit * 2
+  }
+});

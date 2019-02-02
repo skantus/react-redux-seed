73f786651caedb3a89330e4851ade76f142f9ef5
Alejo Castaño <drskantus@hotmail.com>
2019-02-02 16:48:17 +0100

    Use styled-component globally

---
+++ src/global-styles.js
@@ -0,0 +1,10 @@
+// https://www.styled-components.com/docs/faqs#what-do-i-need-to-do-to-migrate-to-v4
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans');
+  body {
+    margin: 0px;
+    font-family: 'Noto Sans';
+  }
+`;
